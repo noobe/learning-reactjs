5f0e3a8cf31228c1adf97f8f4e3c543f6458f872
Sarath <sarath.regedit@gmail.com>
2022-10-28 00:39:17 +0530

    Minor changes

--- 012 - Popular Hooks in React library/1 - useState/app.js
+++ 012 - Popular Hooks in React library/1 - useState/app.js
@@ -1,6 +1,7 @@
 const App = () => {
-  return <button>{props.myLabel}</button>;
+  const [count, setCount] = React.useState(0);
+  return <button onClick={() => { setCount(count+1); }}>{count}</button>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App name="Sreeraj"/>);
+root.render(<App />);
--- 012 - Popular Hooks in React library/2 - useEffect/app.js
+++ 012 - Popular Hooks in React library/2 - useEffect/app.js
@@ -1,6 +1,26 @@
 const App = () => {
-  return <button>{props.myLabel}</button>;
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+
+  });
+
+  React.useEffect(() => {
+
+  }, []);
+
+  React.useEffect(() => {
+
+  }, [count]);
+
+  React.useEffect(() => {
+    return () => {
+      
+    }
+  }, []);
+
+  return <button onClick={() => { setCount(count+1); }}>{count}</button>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App name="Sreeraj"/>);
+root.render(<App />);
